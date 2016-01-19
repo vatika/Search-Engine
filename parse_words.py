93f0087bbd4ed2e9bccdafa4542c7d2651af15be
Vatika Harlalka <vatikaharlalka@gmail.com>
2016-01-19 17:51:46 +0530

    initial Parser working

--- parse_words.py
+++ parse_words.py
@@ -3,30 +3,64 @@
 import xml.sax
 from xml.sax.handler import ContentHandler
 import cgi
+import re
+
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 
 all_articles = []
+stemmer = PorterStemmer()
 
+
+class Article:
+	def __init__(self):
+		title = ""
+		alternate_links = [] #Alternate representations of title 
+		content = ""
+		token = {}
+		citations = []
+		external_links = []
+		id = 0
+
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+
+def tokenize(text):
+	print text
+	token = re.sub('[^a-zA-Z0-9 \n\.]', ' ', text)
+	token = token.split()
+	stems = stem_tokens(token, stemmer)
+	return stems
 
 class WikipediaHandler(xml.sax.ContentHandler):
 	def __init__(self):
-		self.title = ""
 		self.CurrentData = ""
 		self.article_no = 0
-
+	
 	def get_tokens(self, content, title):
+		self.t = title
+		self.article.token = {}
+		self.article.token['title'] = tokenize(title)
+		self.article.token['text'] = tokenize(get_body(content))
+		self.article.token['headings'] = tokenize(get_headings(content))
+		self.article.token['References'] = tokenize(get_references(content))
+		print self.article.token['title']
 
-		token = {}
-		token['title'] = []
-		token['text'] = []
-		token['References'] = []
+	
 
 	def startElement(self, tag, attributes):
 		"""
 		At every <page> parsed, create a new object of class Article
 		""" 
 		self.CurrentData = tag
-		if self.CurrentData == "page":
+		if tag == "page":
 			self.article = Article()
+			self.article.content = ""
 			self.article.id = self.article_no
 			self.article_no += 1
 
@@ -35,35 +69,20 @@ class WikipediaHandler(xml.sax.ContentHandler):
 		Get content of every header
 		"""
 		if self.CurrentData == "title":
-			self.article.title = content.encode('utf-8')
-
-		if self.CurrentData == "text":
-			self.article.content = content
-			tokens = self.get_tokens(self.article.content, self.article.title)
-		# if self.CurrentData == "id":
-		# 	if content != content:
-		# 		self.article.id = int(content)
-		# 	else: self.article.id = None
-
+			self.article.title = content.decode('utf-8')
+			
+		elif self.CurrentData == "text":		
+			self.article.content += content
+			
 	def endElement(self, tag):
-		if self.CurrentData == "title":
-			print "Title: ", self.article.title
-		# if self.CurrentData == "id":
-		# 	print "Id: ", self.article.id
-		if self.CurrentData == "page":
-			all_articles.append(Article)
+		
+		if self.CurrentData == "text":
+			self.get_tokens(self.article.content, self.article.title)
 
-class Article:
-	def __init__(self):
-		title = []
-		# id = ""
-		# parent_id = ""
-		alternate_links = [] #Alternate representations of title 
-		content = ""
-		tokens = []
-		citations = []
-		external_links = []
-		id = 0
+		if tag == "page":
+			all_articles.append(Article)
+		
+		self.CurrentData = ""
 
 
 if ( __name__ == "__main__"):
