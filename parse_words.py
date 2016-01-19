03e131251613acd8f74c0dffd31a1a28f36b24c9
Vatika Harlalka <vatikaharlalka@gmail.com>
2016-01-19 15:01:25 +0530

    Get content

--- parse_words.py
+++ parse_words.py
@@ -6,36 +6,61 @@ import cgi
 
 all_articles = []
 
+
 class WikipediaHandler(xml.sax.ContentHandler):
 	def __init__(self):
 		self.title = ""
 		self.CurrentData = ""
+		self.article_no = 0
+		self.article.content = ""
 		
+	def get_tokens(self):
+
+
 	def startElement(self, tag, attributes):
+		"""
+		At every <page> parsed, create a new object of class Article
+		""" 
 		self.CurrentData = tag
 		if self.CurrentData == "page":
 			self.article = Article()
+			self.article.id = self.article_no
+			self.article_no += 1
 
 	def characters(self, content):
+		"""
+		Get content of every header
+		"""
 		if self.CurrentData == "title":
 			self.article.title = content
-			
+
+		if self.CurrentData = "text":
+			self.article.content = content
+			tokens = get_tokens(self.art)
+		# if self.CurrentData == "id":
+		# 	if content != content:
+		# 		self.article.id = int(content)
+		# 	else: self.article.id = None
+
 	def endElement(self, tag):
 		if self.CurrentData == "title":
 			print "Title: ", self.article.title
+		# if self.CurrentData == "id":
+		# 	print "Id: ", self.article.id
 		if self.CurrentData == "page":
 			all_articles.append(Article)
 
 class Article:
 	def __init__(self):
 		title = []
-		id = ""
-		parent_id = ""
+		# id = ""
+		# parent_id = ""
 		alternate_links = [] #Alternate representations of title 
 		content = ""
 		tokens = []
 		citations = []
 		external_links = []
+		id = 0
 
 
 if ( __name__ == "__main__"):
